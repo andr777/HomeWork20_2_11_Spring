4bbd2fc6df58d003ff2ccef2bbd7a289984511d4
andr777 <andr.root@gmail.com>
2022-10-21 02:19:45 +0300

    Initial commit

---
+++ src/main/java/pro/sky/HomeWork20_2_11_Spring/HomeWork20211SpringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.HomeWork20_2_11_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork20211SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork20211SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/HomeWork20_2_11_Spring/HomeWork20211SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.HomeWork20_2_11_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork20211SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
